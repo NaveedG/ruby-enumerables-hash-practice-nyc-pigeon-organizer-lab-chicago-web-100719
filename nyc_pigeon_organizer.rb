0aacfe69333be1af38217b5598b3e0c6333dc060
Naveed Ganjani <nganjani@protonmail.com>
2019-09-13 17:30:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |firstkey, value_hash|
+      value_hash.each do |secondkey, values|
+        values.each do |value|
+          new_hash[value] = {} if !new_hash[value]
+          new_hash[value][firstkey] = [] if !new_hash[value][firstkey]
+          new_hash[value][firstkey].push(secondkey.to_s)
+        end
+      end
+    end
+  new_hash
 end
