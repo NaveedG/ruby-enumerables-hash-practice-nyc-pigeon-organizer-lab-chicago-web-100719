104e848563a8a4d892f509b364864c52dcd19ac1
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 16:20:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |category, category_hash|
     category_hash.each do |sub_category, names|
-      new_hash[names] = {}
+      names.each do |name|
+        new_hash[name] = {} if !new_hash[name]
     end
   end
   new_hash
