8ad4e343f6d9feb13901cf6bde6ae1614fb20b1d
Naveed Ganjani <nganjani@protonmail.com>
2019-09-14 16:09:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,2 @@
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |firstkey, value_hash|
-      value_hash.each do |secondkey, values|
-        values.each do |value|
-          new_hash[value] = {} if !new_hash[value]
-          new_hash[value][firstkey] = [] if !new_hash[value][firstkey]
-          new_hash[value][firstkey].push(secondkey.to_s)
-        end
-      end
-    end
-  new_hash
 end
